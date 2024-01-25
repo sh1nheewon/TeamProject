3c6838525a296c01a9b74a9306ca4b03ab2e6b4b
sh1nheewon <tlsgmldnjs00@naver.com>
2024-01-25 13:51:32 +0900

    join

--- project/메뉴/join/join.js
+++ project/메뉴/join/join.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     const btn_join = document.querySelector("button.btn_join");
     btn_join.addEventListener("click", () => {
-        // document.location.href = "/홈메뉴.html";
         alert("회원가입 완료");
+        // document.location.href = "/홈메뉴.html"; 
+
     });
 
 });
